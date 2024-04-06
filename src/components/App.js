2baa9afe0c6e94e4042cca52d8dcc02a31ef6f80
Ozair Kamran <ozairkamran11@gmail.com>
2024-04-06 18:34:29 -0500

    setup the project

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import '../css/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a>
+          {/* In order to run this project make sure you are in this directory by using cd in the terminal like cd ratemyhousing.
+              Now once in directory, try running the project in the terminal to help you get used to it by writing npm start in the terminal,
+              and then wait... (the project should run, now play around with things, and start coding) */}
+          Welcome to Rate My Housing! - Start coding!
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
